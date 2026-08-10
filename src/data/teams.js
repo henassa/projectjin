8cf6c37986cc06918d9c292f661428a90d0155bd
henassa <164503585+henassa@users.noreply.github.com>
2026-08-10 12:59:36 +0200

    Update teams.js

--- src/data/teams.js
+++ src/data/teams.js
@@ -260,11 +260,11 @@ export const teams = [
     players: [
       { pseudo: "Colevag", pronouns: "il / he", nationality: "FR-NORM", flagImage: "/flags/FR-NORM.png" },
       { pseudo: "felschrr", pronouns: "il / he", nationality: "FR", flagImage: "/flags/FR.png" },
-      { pseudo: "Kesscro", pronouns: "il / he", nationality: "BE", flagImage: "/flags/BE.png" },
-      { pseudo: "Static2k", pronouns: "il / he", nationality: "FR", flagImage: "/flags/FR.png" },      
+      { pseudo: "Kesscro", pronouns: "il / he", nationality: "BE", flagImage: "/flags/BE.png" },    
       { pseudo: "Liquidz", pronouns: "il / he", nationality: "FR-BR", flagImage: "/flags/FR-BR.png" },
       { pseudo: "Mael", pronouns: "il / he", nationality: "FR", flagImage: "/flags/FR.png" },
       { pseudo: "Maghrebiere", pronouns: "il / he", nationality: "DZ", flagImage: "/flags/DZ.png" },
+      { pseudo: "Static2k", pronouns: "il / he", nationality: "FR", flagImage: "/flags/FR.png" },  
       { pseudo: "steve", pronouns: "il / he", nationality: "FR", flagImage: "/flags/FR.png" },
       { pseudo: "Ulyxx3", pronouns: "il / he", nationality: "FR", flagImage: "/flags/FR.png" },
       { pseudo: "wati", pronouns: "il / he", nationality: "FR", flagImage: "/flags/FR.png" },
